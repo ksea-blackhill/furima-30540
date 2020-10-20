1d09ac381b77966325cfdbfefbc5e2663b09908f
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-20 18:13:55 +0900

    新規登録画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
+  #before_action :move_to_index, except: [:index]
   devise_for :users
- 
+  root to: 'items#index'
+
+
+
+
+
+
+
+
+  #private
+
+  #def move_to_index
+    #unless user_signed_in?
+      #redirect_to action: :index
+    #end
+  #end
 end
+
+
+
