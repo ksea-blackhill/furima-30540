9336c5f7ce59f55b733e2729cee4c785ed313ea8
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-21 16:52:59 +0900

    rubocopの実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {'1Aa' + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    birthday              {Faker::Date.birthday}
-    first_name            {'新庄'}
-    last_name             {'剛志'}
-    kana_first            {'シンジョウ'}
-    kana_last             {'ツヨシ'}
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { '1Aa' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    birthday              { Faker::Date.birthday }
+    first_name            { '新庄' }
+    last_name             { '剛志' }
+    kana_first            { 'シンジョウ' }
+    kana_last             { 'ツヨシ' }
   end
 end
