c3e51ed6c36f069689c76e0c7bd5da691bc74a91
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-21 19:21:14 +0900

    ユーザ管理機能修正３

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,26 +9,6 @@ describe User do
       it '入力必須項目全て存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordが半角英数字混合で6文字以上であれば登録できる' do
-        @user.password = '1234pw'
-        @user.password_confirmation = '1234pw'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名は全角漢字であれば登録できる' do
-        @user.first_name = '信条'
-        @user.last_name  = '剛'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名は全角ひらがなであれば登録できる' do
-        @user.first_name = 'しんじょう'
-        @user.last_name  = 'つよし'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名は全角カタカナであれば登録できる' do
-        @user.first_name = 'シンジョウ'
-        @user.last_name  = 'ツヨシ'
-        expect(@user).to be_valid
-      end
     end
 
     context '新規登録がうまくいかない時' do
