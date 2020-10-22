104d15b11eaf23fe68a46a972e99e00b9fb48bcf
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-22 17:13:42 +0900

    category, status , cost, area, dayモデルの内容記述とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+  
   belongs_to :user
   has_one_attached :image
 
@@ -8,5 +15,13 @@ class Item < ApplicationRecord
     validates :description
     validates :price
     
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :area_id
+    validates :day_id
   end
 end
