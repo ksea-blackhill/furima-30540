f74b8dccc8db3ef477220ad57734d625b8eb8e20
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-23 19:28:28 +0900

    priceの値段上限加減設定のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,4 +24,7 @@ class Item < ApplicationRecord
     validates :area_id
     validates :day_id
   end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+
 end
