f8d796c18fe10f240add6f42b0faadbfdc056ed8
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-23 23:46:31 +0900

    itemモデルの単体テストの実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name        {'商品名'}
+    description {'説明文'}
+    category_id {2}
+    status_id   {2}
+    cost_id     {2}
+    area_id     {2}
+    day_id      {2}
+    price       {300}
+    association :user
   end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload("/files/arrow_top.png")
+  end
+
+
+  describe '商品出品機能' do
+    context '商品出品がうまくいく時' do
+      it '入力項目が全て存在すれば登録できる'do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかない時' do
+      it '商品画像が1枚も添付されていないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと登録できない' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと登録できない'do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが --- のままだと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'status_idが --- のままだと登録できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it 'cost_idが --- のままだと登録できない' do
+        @item.cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Cost must be other than 1")
+      end
+      it 'area_idが --- のままだと登録できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it 'day_idが --- のままだと登録できない' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
+      end
+      it 'priceが空だと登録できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが半角数字以外だと登録できない' do
+        @item.price = "１２３４５"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが299円以下だと登録できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが10,000,000円以上だと登録できない' do
+        @item.price = "100000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
