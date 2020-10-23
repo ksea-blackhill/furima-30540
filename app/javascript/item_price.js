f4ecbb05bcf02941b9aee6a2453bcf72dbd74895
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-23 19:29:45 +0900

    JSのイベント発火実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input",() => {
+    const inputValue = priceInput.value;
+    console.log(inputValue)
+    const addTax = document.getElementById("add-tax-price");
+    addTax.innerHTML = inputValue * 0.1
+    const Profit = document.getElementById("profit");
+    Profit.innerHTML = inputValue - addTax.innerHTML
+  })
+});
