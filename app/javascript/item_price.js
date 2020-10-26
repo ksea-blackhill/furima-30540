4e3d7b17e8972a7ef2388179137600a70f77ca80
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-26 10:59:52 +0900

    不要記述の削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,6 @@ window.addEventListener('mousedown', () => {
   const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input",() => {
     const inputValue = priceInput.value;
-    console.log(inputValue)
     const addTax = document.getElementById("add-tax-price");
     addTax.innerHTML = inputValue * 0.1
     const Profit = document.getElementById("profit");
