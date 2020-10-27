7f5c02afb5b8c0e9e072031976a960cfcecf6dbc
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-27 16:53:31 +0900

    商品情報編集機能の実装修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit,:show,:update]
+  before_action :move_to_root, only: :edit
 
   def index
     @item = Item.all.order(created_at: 'DESC')
@@ -18,17 +20,13 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def show
-    @item = Item.find(params[:id])
+  def show  
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,4 +39,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :cost_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_root
+      redirect_to root_path unless current_user == @item.user
+  end
+  
+
 end
