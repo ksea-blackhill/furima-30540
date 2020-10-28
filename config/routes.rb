e2f82d178be0d25b98137a9392ef29381ad0eccc
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-28 18:11:26 +0900

    ルーティング書き直し

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items
+  resources :items do
+    resources :payments, only: [:index,:create]
+  end
 end
