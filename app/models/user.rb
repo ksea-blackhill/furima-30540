fc293e12ed9a732afa66d7c8d3c082a2db7a3a3b
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-11-01 16:47:00 +0900

    商品購入機能の修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  has_one  :payment
+  has_many :payments
 
   with_options presence: true do
     validates :nickname
